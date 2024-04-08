3495449701204a3a8a1eeaaaa4c6eb5f1b5fe23e
ijrafeal <irvin999rafael6@csu.fullerton.edu>
2024-04-08 10:41:55 -0700

    fixed the bug with the waves, also added money system so user gets money when beating an enemy

--- Barrage/Assets/Scripts/Health.cs
+++ Barrage/Assets/Scripts/Health.cs
@@ -6,6 +6,7 @@ public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private int hitPoints = 2; //hitpoints
+    [SerializeField] private int moneyValue = 50;
 
     private bool isDestroyed = false;
     public void TakeDmg(int dmg) 
@@ -15,6 +16,7 @@ public class Health : MonoBehaviour
         if (hitPoints <= 0 && !isDestroyed) 
         {
             EnemySpawner.onEnemyDestroyed.Invoke();
+            LevelManager.main.IncreaseMoney(moneyValue);
             isDestroyed = true;
             Destroy(gameObject);
         }
--- Barrage/Assets/Scripts/LevelManager.cs
+++ Barrage/Assets/Scripts/LevelManager.cs
@@ -8,8 +8,35 @@ public class LevelManager : MonoBehaviour
 
     public Transform startPoint;
     public Transform[] path;
+
+    public int money;
+
+
     private void Awake()
     {
         main = this;
+    }
+
+    private void Start()
+    {
+        money = 100;
+    }
+
+    public void IncreaseMoney(int amount) 
+    { 
+        money += amount;
+    }
+
+    public bool SpendMoney(int amount)
+    {
+        if (money >= amount) 
+        {
+            money -= amount;
+            return true;
+        } else
+        {
+            Debug.Log("Not enough Money");
+            return false;
+        }
     }
 }
