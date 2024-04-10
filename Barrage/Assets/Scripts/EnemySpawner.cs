3cb3bc5b5c599fa768f417e45c2cdefdf5269b6d
ijrafeal <irvin999rafael6@csu.fullerton.edu>
2024-04-10 14:17:02 -0700

    added a tankier enemy type, bug found where projectiles don't destroy after a certain range

--- Barrage/Assets/Scripts/EnemySpawner.cs
+++ Barrage/Assets/Scripts/EnemySpawner.cs
@@ -13,6 +13,7 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private float enemiesPerSecond = 0.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
     [SerializeField] private float difficulty = 0.75f;
+    [SerializeField] private float maxEPS = 12f; //maximum enemies per second
 
     [Header("Events")]
     public static UnityEvent onEnemyDestroyed = new UnityEvent();
@@ -21,6 +22,7 @@ public class EnemySpawner : MonoBehaviour
     private float timeSinceLastSpawn;
     private int enemiesAlive;
     private int enemiesLeft;
+    private float eps; //enemies per second
     private bool isSpawning = false;
 
     private void Awake()
@@ -38,7 +40,7 @@ public class EnemySpawner : MonoBehaviour
 
         timeSinceLastSpawn += Time.deltaTime;
 
-        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeft > 0)
+        if (timeSinceLastSpawn >= (1f / eps) && enemiesLeft > 0)
         {
             SpawnEnemy();
             enemiesLeft--;
@@ -63,6 +65,8 @@ public class EnemySpawner : MonoBehaviour
 
         isSpawning = true;
         enemiesLeft = EnemiesPerWave();
+
+        eps = EnemiesPerSecond();
     }
 
     private void EndWave() 
@@ -73,7 +77,8 @@ public class EnemySpawner : MonoBehaviour
         StartCoroutine(StartWave());
     }
     private void SpawnEnemy() {
-        GameObject prefabToSpawn = enemyPrefabs[0];
+        int index = Random.Range(0, enemyPrefabs.Length);
+        GameObject prefabToSpawn = enemyPrefabs[index];
         Instantiate(prefabToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
     }
     private int EnemiesPerWave() 
@@ -81,4 +86,8 @@ public class EnemySpawner : MonoBehaviour
         return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficulty));
     }
 
+    private float EnemiesPerSecond()
+    {
+        return Mathf.Clamp(enemiesPerSecond * Mathf.Pow(currentWave, difficulty), 0, maxEPS);
+    }
 }
